553a53a612e564e664150a95866b5c4bc62e0bf6
Steve Farra <32561115+boyg@users.noreply.github.com>
2018-05-05 02:26:04 -0700

    Update p6.py

--- p6.py
+++ p6.py
@@ -1,4 +1,7 @@
 n = 100
+
+'''Using Faulhaber's formula for the p = 1 and p = 2 cases'''
 sum_of_squares = n * (n+1) * (2*n+1) / 6
 square_of_sum = (n * (n+1) / 2) ** 2
+
 print(square_of_sum - sum_of_squares)
