3742e61eee0abefa3b12eddee3b4ea93b20e81b7
Steve Farra <32561115+boyg@users.noreply.github.com>
2018-05-05 02:24:07 -0700

    Update p6.py

--- p6.py
+++ p6.py
@@ -1,3 +1,4 @@
-sum_of_squares = 100 * (100+1) * (2*100+1) / 6
-square_of_sum = (100 * (100+1) / 2) ** 2
+n = 100
+sum_of_squares = n * (n+1) * (2*n+1) / 6
+square_of_sum = (n * (n+1) / 2) ** 2
 print(square_of_sum - sum_of_squares)
