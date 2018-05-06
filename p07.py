678d8e8a81d60563ecd9b8690f331c4a80455e74
Steve Farra <st.farra@gmail.com>
2018-05-06 00:22:19 -0700

    Renamed files and condensed some comments

---
+++ p07.py
@@ -0,0 +1,23 @@
+'''
+What is the 10001st prime number?
+'''
+from num_theory import sieve_of_eratosthenes
+from math import log, ceil
+
+'''nth prime'''
+n = 10001
+
+'''
+Use the prime number theorem 
+to place an upper bound on the nth prime number
+'''
+upper_bound = int(ceil(n * (log(n) + log(log(n)))))
+
+sieve = sieve_of_eratosthenes(upper_bound)
+primes = []
+
+for i in range(2, upper_bound+1):
+    if sieve[i] == True:
+        primes.append(i)
+
+print(primes[n-1])
